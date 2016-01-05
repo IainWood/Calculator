145019582d3d383a4e2ee7ce3d1920a6eb085094
Iain Woodburn <enjkw13@gmail.com>
2016-01-05 13:43:21 -0500

    Create CalculatorViewInterface.java

---
+++ CalculatorViewInterface.java
@@ -0,0 +1,11 @@
+package edu.purdue.iwoodbur.calculator;
+
+/**
+ * Created by Iain Woodburn on 02-Dec-15.
+ */
+public interface CalculatorViewInterface {
+
+    void display(String val);
+    void invalid();
+
+} //end CalculatorViewInterface
